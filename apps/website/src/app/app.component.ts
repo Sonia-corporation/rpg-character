70bd47935d81dc2eebda9c5414fa8d863acc4949
TESTELIN Geoffrey <geoffrey.testelin@gmail.com>
2021-11-29 00:07:20 +0100

    chore: configure nx

---
+++ apps/website/src/app/app.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'rpg-character-root',
+  styleUrls: ['./app.component.scss'],
+  templateUrl: './app.component.html',
+})
+export class AppComponent {}
---
+++ apps/website/src/app/app.module.ts
@@ -0,0 +1,10 @@
+import { AppComponent } from './app.component';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+@NgModule({
+  bootstrap: [AppComponent],
+  declarations: [AppComponent],
+  imports: [BrowserModule],
+})
+export class AppModule {}
